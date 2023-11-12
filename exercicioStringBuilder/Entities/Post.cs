a00fc9271e6413dcb1052b12348bd2681f578e02
Bruno Silva <bruno.s.iwamura@gmail.com>
2023-11-12 09:35:00 -0300

    finalizando programa

--- exercicioStringBuilder/Entities/Post.cs
+++ exercicioStringBuilder/Entities/Post.cs
@@ -35,5 +35,23 @@ namespace exercicioStringBuilder.Entities
         {
             Comments.Remove(comment);
         }
+
+        public override string ToString()
+        {
+            //Vamos utilizar a classe StringBuilder para realizar esse codigo de maneira mais eficiente
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments:");
+
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
     }
 }
